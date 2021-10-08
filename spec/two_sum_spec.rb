f23a1e05b59da5a486d848ce4a6852e4b74a105e
Jose Lopez <joselopez11394@gmail.com>
2021-10-08 18:17:31 -0400

    adds more test for two sum challenge

--- spec/two_sum_spec.rb
+++ spec/two_sum_spec.rb
@@ -11,13 +11,23 @@ describe 'should return an array of 2 numbers that add up to the target number' 
     expect(results).to include(1)
   end
 
-  it 'should return index of [2,6]' do
+  it 'should return index of [1,4]' do
     nums = [2,7,11,15,7,5,3]
     target = 14
 
     results = two_sum(nums, target)
+    
+    expect(results).to include(1)
+    expect(results).to include(4)
+  end
 
-    expect(results).to include(2)
-    expect(results).to include(6)
+  it 'should return index of [0,1]' do
+    nums = [3,3]
+    target = 6
+
+    results = two_sum(nums, target)
+
+    expect(results).to include(0)
+    expect(results).to include(1)
   end
 end
